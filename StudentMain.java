f8eb3e2a676cd6c8845688409defb024cb2a1e76
Amulya-03 <ammuluyadav100@gmail.com>
2025-05-09 19:47:12 +0530

    Add files via upload

---
+++ StudentMain.java
@@ -0,0 +1,10 @@
+package com.blc.elc;
+
+public class StudentMain {
+	
+	public static void main (String [] args){
+		 Student s = new Student();
+		 s.setStudentDetails("ABC", 101, 85.5);
+		 System.out.println(s.getStudentDetails());
+		 }
+	}
